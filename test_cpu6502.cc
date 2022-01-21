c867e6cbcb1ef70bcacecc113efec6e16d3103cb
Gabriel Correia <gabrielcorreia01012020@hotmail.com>
2022-01-21 17:24:35 -0300

    Using 4 spaces instead of tabs and using libfmt instead of the format c++ header

--- test_cpu6502.cc
+++ test_cpu6502.cc
@@ -1,31 +1,34 @@
 #include <memory>
 #include <iostream>
-#include <format>
+
+#include <fmt/format.h>
 
 #include "cpu6502.hh"
 
-static uint8_t cpu_ram[MAX_RAM_STORAGE];
-static const uint8_t cpu_rom[MAX_ROM_STORAGE];
+static uint8_t cpu_ram[MAX_RAM_STORAGE]{};
+static const uint8_t cpu_rom[MAX_ROM_STORAGE]{};
 
 uint8_t cpu_6502_read (uint16_t address)
 {
-	if (address < MAX_RAM_STORAGE)
-		return cpu_ram[address];
-	return cpu_rom[address];
+    if (address < MAX_RAM_STORAGE)
+        return cpu_ram[address];
+    return cpu_rom[address];
 }
 
 void cpu_6502_write (uint16_t address, uint8_t data)
 {
-	cpu_ram[address] = data;
+    cpu_ram[address] = data;
 }
 
 int main ()
 {
-	auto cpu_6502 = std::make_shared<cpu6502> (cpu_6502_read, cpu_6502_write);
-	uint64_t executed_cycles = 0;
+    auto cpu_6502 = std::make_shared<cpu6502> (cpu_6502_read, cpu_6502_write);
+    uint64_t executed_cycles = 0;
 
-	cpu_6502->reset ();
-	auto [executed, bytes_used] = cpu_6502->clock (14, executed_cycles);
-	std::cout << std::format ("Executed instructions: {}, Bytes read: {}", executed, bytes_used) << std::endl;
-	cpu_6502->printcs ();
+    cpu_6502->reset ();
+    auto [executed, bytes_used] = cpu_6502->clock (14, executed_cycles);
+    fmt::print ("Executed instructions: {}, Bytes read: {}\n", executed, bytes_used);
+    cpu_6502->printcs ();
+
+    return 0;
 }
